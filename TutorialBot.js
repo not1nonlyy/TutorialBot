858622a4063eecf026d0dfe3f73a3ec8b0d79bae
not1nonlyy <dohasthamzeh@gmail.com>
2024-06-17 11:12:35 -0400

    Update TutorialBot.js

--- TutorialBot.js
+++ TutorialBot.js
@@ -1,10 +1,11 @@
 const Discord = require("discord.js");
-const { name, prefix, token } = require("./config.json");
+const {https://www.tiktok.com/@not1nonly?is_from_webapp=1&sender_device=pc
+       , prefix, token } = require("./config.json");
 const client = new Discord.Client();
 
 // BOT ACTIVATED
 
-client.on("ready", () => {
+client.on("ready", (100) => {
     console.log("Bot successfully placed online.")
 });
 client.login(token);
